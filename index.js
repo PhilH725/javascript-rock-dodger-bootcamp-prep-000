b5807d21c8745a5580e95ecdcd2d2ce118df019b
Phil Hartley <philh725@gmail.com>
2018-10-18 03:54:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -85,14 +85,14 @@ function createRock(x) {
      rock.style.top = `${top+=4}px`
      if (checkCollision(rock) === true) {
        endGame();
-     } 
-     if (top < 380) {
+     } else if (top < 380) {
        //rock.style.top = `${top+=4}px`
        window.requestAnimationFrame(moveRock);
      } else {
-       console.log(ROCKS)
-       GAME.removeChild(rock)
-       ROCKS.shift()
+       rock.remove()
+       //console.log(ROCKS)
+       //GAME.removeChild(rock)
+       //ROCKS.shift()
      }
 
     /**

20f6d2679e0b10a834f69220b8e67f43fc2a340e
Phil Hartley <philh725@gmail.com>
2018-10-18 03:56:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -123,11 +123,11 @@ function createRock(x) {
  */
 function endGame() {
   clearInterval(gameInterval)
-  ROCKS.forEach(function(rock) { rock.remove() })
-  //while (ROCKS.length > 0) {
-    //GAME.removeChild(ROCKS[0]);
-    //ROCKS.shift();
-  //}
+  //ROCKS.forEach(function(rock) { rock.remove() })
+  while (ROCKS.length > 0) {
+    GAME.removeChild(ROCKS[0]);
+    ROCKS.shift();
+  }
   /*for (let i = 0; i < ROCKS.length; i++) {
     GAME.removeChild(ROCKS[i])
   } */
